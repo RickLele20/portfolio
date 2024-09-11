7299f2bc5278e2efd3b4bd1c3f5d54886827b261
PeriquitoAvoado <ricklele10@gmail.com>
2024-09-11 08:44:49 -0300

    portfolio

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Rolagem suave para os links de navegação
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
